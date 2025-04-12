f36558f116c0579680a0f475df274ed3a8e80ee3
SunSunny1210 <sol16821@gmail.com>
2025-04-12 12:18:44 +0200

    Do iteration 1.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,66 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
-
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
+import contactsInfo from "./contacts.json";
+import { ref } from "vue";
+
+const contacts = ref(contactsInfo.slice(0, 5));
 </script>
 
+
+<template>
+    <div class="app">
+        <h1>IronContacts</h1>
+        <table>
+        <tr>
+            <td class="title">Picture</td>
+            <td class="title">Name</td>
+            <td class="title">Popularity</td>
+        </tr>
+        <tr v-for="(contact , i) in contacts" :key="i">
+            <td>
+                <img :src="contact.pictureUrl" />
+            </td>
+            <td>{{ contact.name }}</td>
+            <td>{{ parseFloat(contact.popularity).toFixed(2) }}</td>
+        </tr>
+    </table>
+    </div>
+</template>
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: sans-serif;
+}
+
+.app {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    h1 {
+        margin-top: 1rem;
+        padding: 2rem;
+        font-size: 3rem;
+    }
+
+    .title {
+        padding: 1rem;
+        font-size: 2rem;
+        font-weight: bold;
+        text-align: center;
+    }
+
+    td {
+        font-size: 1.7rem;
+        text-align: center;
+    }
+
+    img {
+    height: 10rem;
+    }
 }
 </style>
