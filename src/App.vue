801789c00170abf536d75ad50a1741eede069744
SunSunny1210 <sol16821@gmail.com>
2025-04-12 13:04:31 +0200

    Fix iteration 3.

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,16 @@ import { ref } from "vue";
 
 const contacts = ref(contactsInfo.slice(5, 10));
 
-const randomContact = ref([])
-
 const addRandomContact = () => {
-    const newRandomContact = contactsInfo[Math.floor(Math.random() * contactsInfo.length)];
+    const randomContact = contactsInfo[Math.floor(Math.random() * contactsInfo.length)];
 
-    if (!randomContact.value.includes(newRandomContact)) {
-        randomContact.value.push(newRandomContact)
+    if (!contacts.value.includes(randomContact)) {
+        contacts.value.push({...randomContact})
     }
+}
 
+const sortByName = () => {
+    contacts
 }
 </script>
 
@@ -20,7 +21,11 @@ const addRandomContact = () => {
 <template>
     <div class="app">
         <h1>IronContacts</h1>
-        <button @click="addRandomContact">Add Contact Button</button>
+        <div class="btns">
+            <button @click="addRandomContact">Add Contact Button</button>
+            <button @click="sortByName">Sort by Name</button>
+            <button @click="sortByPopularity">Sort by Popularity</button>
+        </div>
         <table>
         <tr>
             <td class="title">Picture</td>
@@ -28,15 +33,6 @@ const addRandomContact = () => {
             <td class="title">Popularity</td>
             <td class="title">Won an Oscar</td>
             <td class="title">Won an Emmy</td>
-        </tr>
-        <tr v-for="(contact, i) in randomContact" :key="'random-' + i">
-            <td>
-                <img :src="contact.pictureUrl" />
-            </td>
-            <td>{{ contact.name }}</td>
-            <td>{{ parseFloat(contact.popularity).toFixed(2) }}</td>
-            <td v-if="contact.wonOscar">🏆</td>
-            <td v-if="contact.wonEmmy">🏆</td>
         </tr>
         <tr class="contacts-list" v-for="(contact , i) in contacts" :key="i">
             <td>
