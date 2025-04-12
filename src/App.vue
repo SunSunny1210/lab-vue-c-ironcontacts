415935b03fb837f02c88944ba9ed6215d6297f13
SunSunny1210 <sol16821@gmail.com>
2025-04-12 12:56:58 +0200

    Do iteration 3.

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,24 @@ import contactsInfo from "./contacts.json";
 import { ref } from "vue";
 
 const contacts = ref(contactsInfo.slice(5, 10));
+
+const randomContact = ref([])
+
+const addRandomContact = () => {
+    const newRandomContact = contactsInfo[Math.floor(Math.random() * contactsInfo.length)];
+
+    if (!randomContact.value.includes(newRandomContact)) {
+        randomContact.value.push(newRandomContact)
+    }
+
+}
 </script>
 
 
 <template>
     <div class="app">
         <h1>IronContacts</h1>
+        <button @click="addRandomContact">Add Contact Button</button>
         <table>
         <tr>
             <td class="title">Picture</td>
@@ -17,7 +29,16 @@ const contacts = ref(contactsInfo.slice(5, 10));
             <td class="title">Won an Oscar</td>
             <td class="title">Won an Emmy</td>
         </tr>
-        <tr v-for="(contact , i) in contacts" :key="i">
+        <tr v-for="(contact, i) in randomContact" :key="'random-' + i">
+            <td>
+                <img :src="contact.pictureUrl" />
+            </td>
+            <td>{{ contact.name }}</td>
+            <td>{{ parseFloat(contact.popularity).toFixed(2) }}</td>
+            <td v-if="contact.wonOscar">🏆</td>
+            <td v-if="contact.wonEmmy">🏆</td>
+        </tr>
+        <tr class="contacts-list" v-for="(contact , i) in contacts" :key="i">
             <td>
                 <img :src="contact.pictureUrl" />
             </td>
